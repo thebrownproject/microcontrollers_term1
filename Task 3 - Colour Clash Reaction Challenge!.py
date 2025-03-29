444e9ee94e5543f1e7cba824bb1b1884b18f5bb4
thebrownproject <fraserbrown@live.com>
2025-03-29 18:59:18 +1100

    Restructured folders

---
+++ Task 3 - Colour Clash Reaction Challenge!.py
@@ -0,0 +1,111 @@
+from machine import Pin, PWM, ADC
+import time
+import random
+
+leds = [
+    Pin(0, Pin.OUT),
+    Pin(1, Pin.OUT),
+    Pin(2, Pin.OUT)
+    ]
+
+buttons = [
+    Pin(10, Pin.IN, Pin.PULL_UP),
+    Pin(11, Pin.IN, Pin.PULL_UP),
+    Pin(12, Pin.IN, Pin.PULL_UP)
+    ]
+
+round_no = 0
+active_led = 0
+player_selection = 0
+
+buzzer = PWM(Pin(7))
+
+def buzzer_start_game():
+    buzzer.duty_u16(20000)  # Set duty cycle for sound output
+    frequencies = [659, 659, 0, 659, 0, 523, 659, 0, 784, 0, 0, 0, 392, 0, 0, 0]
+    
+    for freq in frequencies:
+        if freq == 0:
+            buzzer.duty_u16(0)
+        else:
+            buzzer.freq(freq)  # Set frequency for buzzer
+            buzzer.duty_u16(20000)
+        
+        time.sleep(0.15)
+    
+    buzzer.duty_u16(0)
+    
+def buzzer_next_round():
+    buzzer.duty_u16(20000)  # Set duty cycle for sound output
+    frequencies = [300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900]
+    
+    for freq in frequencies:
+        buzzer.freq(freq)  # Set frequency for buzzer
+        time.sleep(0.05)
+    
+    buzzer.duty_u16(0)
+
+def buzzer_win():
+    buzzer.duty_u16(20000)
+    frequencies = [(660,0.1),(660,0.1),(0,0.1),(660,0.1),(0,0.1),(510,0.1),(660,0.1),(770,0.1),(0,0.1),(380,0.1),
+(0,0.1),(510,0.1),(0,0.1),(380,0.1),(320,0.1),(0,0.1)]
+    
+    for freq, times in frequencies:
+        if freq == 0:
+            time.sleep(times)
+        else:
+            buzzer.freq(freq)
+            time.sleep(times)
+    
+    buzzer.duty_u16(0)
+    
+#def buzzer_lose():
+
+
+
+
+
+# randon rgb shown
+def light_flash():
+    global active_led
+    random_index = random.randint(0, 2)
+    leds[random_index].on()
+    time.sleep(1)
+    leds[random_index].off()
+    active_led = random_index
+    player_turn()
+
+
+def player_turn():
+    global player_selection
+    while True:
+        for i in range(len(buttons)):
+            if buttons[i].value() == 0:
+                print(f"Button {i} pressed!")
+                player_selection = i
+                check_win()
+                return
+
+def check_win():
+    global round_no, active_led, player_selection
+    if active_led == player_selection:
+        round_no += 1
+        if round_no == 3:
+            print("You have won the game champ!")
+            buzzer_win()
+            return
+        print(f"Correct! Lets move to round {round_no}")
+        buzzer_next_round()
+        time.sleep(.5)
+        light_flash()
+    else:
+        print("Game over!")
+
+buzzer_start_game()
+light_flash()
+
+
+# if button.value() == 0 and not toggle:
+#        toggle = True
+#        print("🚸 Pedestrian button pressed!")
+#        time.sleep(0.2)  # Prevents button bouncing
